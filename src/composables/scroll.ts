72bb94133a1f8e1253a4748da612a40b123945ea
xianyu <xianyu@iiisland.com>
2021-05-13 14:30:37 +0800

    update

--- src/composables/scroll.ts
+++ src/composables/scroll.ts
@@ -12,9 +12,7 @@ const handleScroll = () => {
     // 变量scrollHeight是滚动条的总高度
     const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
     // 滚动条到底部的条件
-    console.log(`scrollTop: ${scrollTop}, windowHeight: ${windowHeight}, scrollHeight: ${scrollHeight}`)
-    if (scrollTop + windowHeight == scrollHeight) {
-        console.log('滚动:', scrollHeight)
+    if (scrollTop + windowHeight + 600 >= scrollHeight) {
         h()
     }
 }
@@ -25,6 +23,5 @@ export function useScroll(handler: scrollHandlerFunc) {
     }
     h = handler
     const dom = document.querySelector('main') || window
-    console.log('开启监听滚动:',dom)
     dom.addEventListener('scroll', handleScroll)
 }
